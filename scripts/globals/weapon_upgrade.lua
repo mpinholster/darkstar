18ffe10c26c28cbe1f9eb6862dd942cda4055ab2
edwardcd <edwardcd@gmail.com>
2017-02-24 00:40:42 -0800

    added PLD
    added PLD

--- scripts/globals/weapon_upgrade.lua
+++ scripts/globals/weapon_upgrade.lua
@@ -3,12 +3,37 @@
 ------------------------------------------------------------------------------------------------------------------------
 Artifact_Weapon_Plus_one={
     --A,  {AF+1 ID, Relic_Armor ID    , Relic_Armor_-1 ID, Crafted_Item ID, Currency ID, curencynNbr}
-    101    ,{    15245    ,    15072    ,    2033    ,    1990    ,    1455    ,    28    }, --war
-    102    ,{    14500    ,    15087    ,    2034    ,    1990    ,    1455    ,    28    },
-    103    ,{    14909    ,    15102    ,    2035    ,    1990    ,    1455    ,    28    },
-    104    ,{    15580    ,    15117    ,    2036    ,    1990    ,    1455    ,    28    },
-    105    ,{    15665    ,    15132    ,    2037    ,    1990    ,    1455    ,    28    },
+    101    ,{    xxxxx    ,    16678    ,    16673    ,    16650    ,    1126    ,    10    },--war lv 50
+    102    ,{    yyyyy    ,    xxxxx    ,        ,        ,    1126    ,    20    },--war lv 60
+    103    ,{    zzzzz    ,    yyyyy    ,        ,        ,    1126    ,    30    },--war lv 70
+    104    ,{    aaaaa    ,    zzzzz    ,        ,        ,    1126    ,    40    },--war lv 75
+    105    ,{    aaaaa    ,    zzzzz    ,        ,        ,    1126    ,    40    },--war lv 75
+    
+    --A,  {AF+1 ID, Relic_Armor ID    , Relic_Armor_-1 ID, Crafted_Item ID, Currency ID, curencynNbr}
+    106    ,{    xxxxx    ,   17643     ,    16820    ,    16545    ,    1126    ,    10    },--PLD lv 50
+    107    ,{    yyyyy    ,    xxxxx    ,    16821    ,    16538    ,    1126    ,    20    },--PLD lv 60
+    108    ,{    zzzzz    ,    yyyyy    ,    16604    ,    16549    ,    1126    ,    30    },--PLD lv 70
+    109    ,{    aaaaa    ,    zzzzz    ,    17651    ,    16578    ,    1126    ,    40    },--PLD lv 75
+    110    ,{    18997    ,    aaaaa    ,    17652    ,     1298    ,    1127    ,    30    },--PLD lv 75
+    
+    --A,  {AF+1 ID, Relic_Armor ID    , Relic_Armor_-1 ID, Crafted_Item ID, Currency ID, curencynNbr}
+    111    ,{    xxxxx    ,   17643     ,    16820    ,    16545    ,    1126    ,    10    },--THF lv 50
+    112    ,{    yyyyy    ,    xxxxx    ,    16821    ,    16538    ,    1126    ,    20    },--THF lv 60
+    113    ,{    zzzzz    ,    yyyyy    ,    16604    ,    16549    ,    1126    ,    30    },--THF lv 70
+    114    ,{    aaaaa    ,    zzzzz    ,    17651    ,    16578    ,    1126    ,    40    },--THF lv 75
+    115    ,{    aaaaa    ,    zzzzz    ,    17651    ,    16578    ,    1126    ,    40    },--THF lv 75
+    };
 
+    
+    
+    -- 1126 BS
+    -- 1127 KS
+    --xxxxx = Razor Axe +1
+    --yyyyy = Razor Axe +2
+    --zzzzz = Razor Axe +3
+    --aaaaa = Razor Axe +4
+    
+    -- Item ID 1126 = Beastmen's Seal
 
     --Example
     --101    ,{    15245    ,    15072    ,    2033    ,    1990    ,    1455    ,    28    }, --war
