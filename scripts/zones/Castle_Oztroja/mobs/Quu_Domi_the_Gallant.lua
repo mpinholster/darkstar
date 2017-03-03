daaa69e6b22fb12ecf62ad24b221510742b14636
mpinholster <m_pinholster@msn.com>
2017-03-03 12:24:16 -0700

    Update Quu_Domi_the_Gallant.lua

--- scripts/zones/Castle_Oztroja/mobs/Quu_Domi_the_Gallant.lua
+++ scripts/zones/Castle_Oztroja/mobs/Quu_Domi_the_Gallant.lua
@@ -9,7 +9,7 @@
 -----------------------------------
 
 function onMobSpawn(mob)
-    if (math.random(1,100) <= 7) then -- Hardcoded "this or this item" drop rate until implemented.
+    if (math.random(1,100) <= 25) then -- Hardcoded "this or this item" drop rate until implemented.
         SetDropRate(2916,15737,1000); -- Sarutobi Kyahan
         SetDropRate(2916,16820,0);
     else
